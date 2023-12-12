135d948dbd179b83fa5c854da98107995950a9ba
smalake <41889150+smalake@users.noreply.github.com>
2023-12-12 14:07:17 +0900

    Docker+Echo+Airの環境構築

---
+++ Makefile
@@ -0,0 +1,8 @@
+up:
+	docker-compose up -d
+stop:
+	docker-compose stop
+down:
+	docker-compose down
+logs:
+	docker-compose logs
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	// Import Echo v4.
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// Create an Echo instance.
+	e := echo.New()
+
+	// Set Route to helloWorld.
+	e.GET("/", helloWorld)
+
+	// Start an Echo server.
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handler
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!!!")
+}
