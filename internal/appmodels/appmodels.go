c67d53fdab3cdb3e4c7d69db4e415e3786c4c26c
smalake <41889150+smalake@users.noreply.github.com>
2023-12-29 12:22:51 +0900

    表示順をパッケージ毎に整理

--- internal/appmodels/appmodels.go
+++ internal/appmodels/appmodels.go
@@ -15,98 +15,7 @@ func New(mysqlCli *mysql.Client) *AppModel {
 	return &AppModel{mysqlCli: mysqlCli}
 }
 
-func (a AppModel) AuthCode(ctx echo.Context) error {
-
-	return nil
-}
-
-// GetDisplayName converts echo context to params.
-func (a AppModel) GetDisplayName(ctx echo.Context) error {
-
-	return nil
-}
-
-// UpdateDisplayName converts echo context to params.
-func (a AppModel) UpdateDisplayName(ctx echo.Context) error {
-
-	return nil
-}
-
-// GetAllEvent converts echo context to params.
-func (a AppModel) GetAllEvent(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// CreateEvent converts echo context to params.
-func (a AppModel) CreateEvent(ctx echo.Context) error {
-
-	return nil
-}
-
-// DeleteEvent converts echo context to params.
-func (a AppModel) DeleteEvent(ctx echo.Context, id int) error {
-	// ------------- Path parameter "id" -------------
-
-	return nil
-}
-
-// GetOneEvent converts echo context to params.
-func (a AppModel) GetOneEvent(ctx echo.Context, id int) error {
-	return nil
-}
-
-// UpdateEvent converts echo context to params.
-func (a AppModel) UpdateEvent(ctx echo.Context, id int) error {
-	return nil
-}
-
-// GetParentName converts echo context to params.
-func (a AppModel) GetParentName(ctx echo.Context, group string) error {
-	return nil
-}
-
-// HealthCheck converts echo context to params.
-func (a AppModel) HealthCheck(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// Invite converts echo context to params.
-func (a AppModel) Invite(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// IsParent converts echo context to params.
-func (a AppModel) IsParent(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// Join converts echo context to params.
-func (a AppModel) Join(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// LoginCheck converts echo context to params.
-func (a AppModel) LoginCheck(ctx echo.Context) error {
-
-	return nil
-}
-
-// LoginGoogle converts echo context to params.
+// Auth関連
 func (a AppModel) LoginGoogle(ctx echo.Context) error {
 	var err error
 
@@ -114,13 +23,11 @@ func (a AppModel) LoginGoogle(ctx echo.Context) error {
 	return err
 }
 
-// LoginMail converts echo context to params.
 func (a AppModel) LoginMail(ctx echo.Context) error {
 
 	return ctx.JSON(http.StatusOK, "ok")
 }
 
-// Logout converts echo context to params.
 func (a AppModel) Logout(ctx echo.Context) error {
 	var err error
 
@@ -128,56 +35,6 @@ func (a AppModel) Logout(ctx echo.Context) error {
 	return err
 }
 
-// GetPattern converts echo context to params.
-func (a AppModel) GetPattern(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// RegisterPattern converts echo context to params.
-func (a AppModel) RegisterPattern(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// DeletePattern converts echo context to params.
-func (a AppModel) DeletePattern(ctx echo.Context, id int) error {
-	return nil
-}
-
-// UpdatePattern converts echo context to params.
-func (a AppModel) UpdatePattern(ctx echo.Context, id int) error {
-	return nil
-}
-
-// GetAllPrivate converts echo context to params.
-func (a AppModel) GetAllPrivate(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshalled arguments
-	return err
-}
-
-// DeletePrivate converts echo context to params.
-func (a AppModel) DeletePrivate(ctx echo.Context, id int) error {
-	return nil
-}
-
-// GetOnePrivate converts echo context to params.
-func (a AppModel) GetOnePrivate(ctx echo.Context, id int) error {
-	return nil
-}
-
-// UpdatePrivate converts echo context to params.
-func (a AppModel) UpdatePrivate(ctx echo.Context, id int) error {
-	return nil
-}
-
-// RegisterUser converts echo context to params.
 func (a AppModel) RegisterUser(ctx echo.Context) error {
 	var err error
 
@@ -185,15 +42,13 @@ func (a AppModel) RegisterUser(ctx echo.Context) error {
 	return err
 }
 
-// ResendCode converts echo context to params.
-func (a AppModel) ResendCode(ctx echo.Context) error {
+func (a AppModel) Join(ctx echo.Context) error {
 	var err error
 
 	// Invoke the callback with all the unmarshalled arguments
 	return err
 }
 
-// Revision converts echo context to params.
 func (a AppModel) Revision(ctx echo.Context) error {
 	var err error
 
@@ -201,8 +56,131 @@ func (a AppModel) Revision(ctx echo.Context) error {
 	return err
 }
 
-// SendMail converts echo context to params.
+func (a AppModel) GetParentName(ctx echo.Context, group string) error {
+	return nil
+}
+
+func (a AppModel) LoginCheck(ctx echo.Context) error {
+
+	return nil
+}
+
+func (a AppModel) AuthCode(ctx echo.Context) error {
+
+	return nil
+}
+
+func (a AppModel) ResendCode(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
+// Event関連
+func (a AppModel) CreateEvent(ctx echo.Context) error {
+
+	return nil
+}
+
+func (a AppModel) GetAllEvent(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
+func (a AppModel) GetOneEvent(ctx echo.Context, id int) error {
+	return nil
+}
+
+func (a AppModel) UpdateEvent(ctx echo.Context, id int) error {
+	return nil
+}
+
+func (a AppModel) DeleteEvent(ctx echo.Context, id int) error {
+	// ------------- Path parameter "id" -------------
+
+	return nil
+}
+
+// Private関連
+func (a AppModel) GetAllPrivate(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
+func (a AppModel) GetOnePrivate(ctx echo.Context, id int) error {
+	return nil
+}
+
+func (a AppModel) UpdatePrivate(ctx echo.Context, id int) error {
+	return nil
+}
+
+func (a AppModel) DeletePrivate(ctx echo.Context, id int) error {
+	return nil
+}
+
+// Pattern関連
+func (a AppModel) GetPattern(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
+func (a AppModel) RegisterPattern(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
+func (a AppModel) UpdatePattern(ctx echo.Context, id int) error {
+	return nil
+}
+
+func (a AppModel) DeletePattern(ctx echo.Context, id int) error {
+	return nil
+}
+
+// Setting関連
+func (a AppModel) GetDisplayName(ctx echo.Context) error {
+
+	return nil
+}
+
+func (a AppModel) UpdateDisplayName(ctx echo.Context) error {
+
+	return nil
+}
+
+func (a AppModel) Invite(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
+func (a AppModel) IsParent(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
 func (a AppModel) SendMail(ctx echo.Context) error {
+	var err error
+
+	// Invoke the callback with all the unmarshalled arguments
+	return err
+}
+
+// HealthCheck
+func (a AppModel) HealthCheck(ctx echo.Context) error {
 	var err error
 
 	// Invoke the callback with all the unmarshalled arguments
