b468b376c60fa3ad8b584d77f2ce333ff374b0e8
smalake <koizumim1002@gmail.com>
2024-07-15 00:52:13 +0900

    クエリのテーブル名修正

--- pkg/database/postgresql/event/query.go
+++ pkg/database/postgresql/event/query.go
@@ -1,4 +1,5 @@
 package event
 
-var EventCreate = "INSERT INTO event (amount, category, store_name, memo, date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)"
-var EventGetAll = "SELECT * FROM event WHERE uid = $1"
+var EventCreate = "INSERT INTO events (amount, category, store_name, memo, date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)"
+var EventGetAll = "SELECT * FROM events LEFT JOIN users ON users.uid = events.group_id WHERE users.uid = $1"
+var EventGetOne = "SELECT * FROM events LEFT JOIN users ON users.uid = events.group_id WHERE users.uid = $1 AND events.id = $2"
